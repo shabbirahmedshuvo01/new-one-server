cd28c1d210d012dd37adb282b90d3e6aa558bebe
Sabbir Shuvo <Shabbir Ahmed Shuvo>
2022-07-01 08:31:29 +0600

    roll one

--- index.js
+++ index.js
@@ -14,6 +14,8 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.khb8i.mongodb.net/?retryWrites=true&w=majority`;
 
 
+// code of client
+
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run() {
